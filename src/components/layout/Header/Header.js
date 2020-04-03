5a8f1acd3aade71c61969be0d8957c57ba7dd808
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2020-04-03 10:14:01 +0200

    Fetched site title through query

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,15 +1,27 @@
 import React from "react"
 import NavigationMenu from "../NavigationMenu"
-import { Link } from "gatsby"
 import css from "./header.module.css"
 import portrait from "../../../images/b_image0.png"
+import { useStaticQuery, Link, graphql } from "gatsby"
 
 const Header = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  console.log(data)
   return (
     <header className={css.header}>
       <Link to="/">
         <h1 className={css.heading} data-img="0">
-          iana godskesen
+          {data.site.siteMetadata.title}
         </h1>
       </Link>
       <NavigationMenu />
